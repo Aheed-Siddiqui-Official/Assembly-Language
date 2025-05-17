6d38c1c711bddbf9bdb20a8f56f3e6febe4d7a3e
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-17 18:21:55 +0500

    commit

--- Lecture 2 Codes/8) printStrings.asm
+++ Lecture 2 Codes/8) printStrings.asm
@@ -8,3 +8,29 @@ dosseg
 msg1 db "hello$"
 msg2 db "world$"
 
+.code
+
+mov ax, @data
+mov ds, ax
+lea dx, msg1
+mov ah, 9
+int 21h
+
+mov dx, 10;for new line
+mov ah, 2
+int 21h
+
+mov dx, 13
+mov ah, 2
+int 21h
+
+lea dx, msg2
+mov ah, 9
+int 21h
+
+mov ah, 4ch
+int 21h
+
+main proc
+main endp
+end main
