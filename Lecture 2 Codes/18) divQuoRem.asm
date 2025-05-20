fe1e027f0918b56e88d7975f06b85bd22e9f2df3
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-05-20 05:49:53 +0500

    commit

--- Lecture 2 Codes/18) divQuoRem.asm
+++ Lecture 2 Codes/18) divQuoRem.asm
@@ -8,3 +8,26 @@ dosseg
 q db ?
 r db ?
 
+.code
+main proc
+
+mov ax, 26
+mov bl, 5
+div bl
+mov q, al
+mov r, ah
+
+mov dl, q
+add dl, 48
+mov ah, 2 
+int 21h
+
+mov dl, r
+add dl, 48
+mov ah, 2
+int 21h
+int ah, 4ch
+int 21h
+ 
+main endp
+end main
